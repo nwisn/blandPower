49bf8b8ac4c0b5c6f5f444ef595fb69c609ec4f0
Nick Wisniewski <dr.nick.wisniewski@gmail.com>
2019-10-08 17:10:04 -0700

    added sample size estimation method

--- R/estimatePowerFromPowerCurve.R
+++ R/estimatePowerFromPowerCurve.R
@@ -1,10 +1,10 @@
 #' Estimate power for Bland-Altman limits of agreement test
 #'
 #' @param powerCurve an object produced by estimatePowerCurve
-#' @param power the pre-determined power
+#' @param n the pre-determined sample size
 #' @return a list
 #' @export
-estimatePower <- function(powerCurve, n){
+estimatePowerFromPowerCurve <- function(powerCurve, n){
   if(!"powerCurve" %in% class(powerCurve)) warning("input is not a powerCurve object")
   if(!n %in% powerCurve$CI.n) warning("The specified n was not evaluated -- finding nearest n")
   powerCurve$power.power[which.min(abs(powerCurve$CI.n - n))]
